ed12acf8ea67467a9ae1d67f864b3bc3fb91903b
Waciuma Wanjohi <code@waciuma.com>
2018-08-16 23:36:49 -0400

    Add database

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import validators
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[validators.DataRequired()])
+    password = PasswordField('Password', validators=[validators.DataRequired(), validators.Length(min=8,max=32)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
